0a80f2744fb21559617e8f6fc96ea5d0f086a663
ella choy <egal21de@gmail.com>
2024-05-03 11:08:16 +0200

    Setup Projekt

---
+++ src/ShoppingApp/ShoppingApp.jsx
@@ -0,0 +1,12 @@
+import AddForm from './components/AddForm';
+
+export default function ShoppingApp() {
+  return (
+    < >
+     <AddForm />
+    </>
+  );
+}
+
+
+
---
+++ src/ShoppingApp/components/AddForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AddForm() {
+  return (
+    <form>
+        <input type="text" placeholder="Add item" />
+        <button>Add</button>
+      
+    </form>
+  )
+}
+
